40fcc5c7b9527a290ef0d6dda943489bd470636d
Vicente Obregon <desarrollogis@gmail.com>
2016-05-28 04:33:55 -0700

    initial

---
+++ main.c
@@ -0,0 +1,26 @@
+/*
+ * File:   main.c
+ * Author: vicente
+ *
+ * Created on May 28, 2016, 3:13 AM
+ */
+
+#include <xc.h>
+
+#pragma config PLLSEL   = PLL3X
+#pragma config FOSC     = INTOSCIO
+#pragma config nPWRTEN  = OFF
+#pragma config WDTEN    = OFF
+#pragma config CCP2MX   = RB3
+#pragma config STVREN   = OFF
+#pragma config CP0      = OFF
+#pragma config CPB      = OFF
+#pragma config WRT0     = OFF
+#pragma config WRTC     = OFF
+#pragma config EBTR0    = OFF
+#pragma config EBTRB    = OFF
+
+void main(void) {
+    while (1) {
+    }
+}
